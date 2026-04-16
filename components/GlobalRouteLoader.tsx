3ead9d63b5a737fb780d328fd432a7ed368fc820
Hamza Koybasi <hamzakybsi@gmail.com>
2026-04-16 10:14:42 +0300

    Use global route loader for all transitions

---
+++ components/GlobalRouteLoader.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import ArchitecturalLoader from "./ArchitecturalLoader";
+
+type GlobalRouteLoaderProps = {
+  logoSrc?: string;
+};
+
+export default function GlobalRouteLoader({ logoSrc }: GlobalRouteLoaderProps) {
+  const pathname = usePathname();
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    setIsVisible(true);
+    const timer = window.setTimeout(() => setIsVisible(false), 1200);
+    return () => window.clearTimeout(timer);
+  }, [pathname]);
+
+  return <ArchitecturalLoader isLoading={isVisible} logoSrc={logoSrc} />;
+}
